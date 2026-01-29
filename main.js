b9a66b47f7d2ac9fad7b8b1af603762ea9773458
Narciso <al02958466@tecmilenio.mx>
2026-01-29 09:32:43 -0600

    main

---
+++ main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+  });
+
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
